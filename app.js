fe97b2a9256db72613f8831886f51ca640fc562a
Daniel Yeh <dyeh1989@gmail.com>
2022-05-05 22:56:08 +0000

    Updated Features 2 & 3

--- app.js
+++ app.js
@@ -13,8 +13,22 @@ if (userCommand === 'read') {
   dataJSON.notes[dataJSON.nextId] = newNote;
   dataJSON.nextId++;
   let stringified = JSON.stringify(dataJSON, null, 2)
-  console.log(stringified);
-  // fs.writeFile();
+  fs.writeFile('data.json', stringified, 'utf8', err => {
+    if (err) throw err;
+  });
+} else if (userCommand === 'delete') {
+  const deleteID = process.argv[3];
+  delete dataJSON.notes[deleteID];
+
+  let stringified = JSON.stringify(dataJSON, null, 2)
+
+  fs.writeFile('data.json', stringified, 'utf8', err => {
+    if (err) throw err;
+  });
+} else if (userCommand === 'update') {
+  const updateID = process.argv[3];
+  const updateContent = process.argv[4];
+
 }
 
 //
@@ -23,7 +37,7 @@ if (userCommand === 'read') {
 //  iterates through .notes, console logs a template
 // node app.js create 'somesortofstring'
 //  increments nextID, adds a new property:value to the notes object
-// node app.js delete
+// node app.js delete 1
 //
 // node app.js update
 //
