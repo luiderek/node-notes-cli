f7d54d6c2b70e54cc8c971593a84b1cfada38d3b
Derek Lui <luiderek@outlook.com>
2022-05-05 23:32:26 +0000

    finishing update function, adding helper function

--- app.js
+++ app.js
@@ -10,36 +10,36 @@ if (userCommand === 'read') {
   }
 } else if (userCommand === 'create') {
   const newNote = process.argv[3];
-  dataJSON.notes[dataJSON.nextId] = newNote;
-  dataJSON.nextId++;
-  let stringified = JSON.stringify(dataJSON, null, 2)
-  fs.writeFile('data.json', stringified, 'utf8', err => {
-    if (err) throw err;
-  });
+  if (newNote) {
+    dataJSON.notes[dataJSON.nextId] = newNote;
+    dataJSON.nextId++;
+  } else {
+    console.error('New notes need content.')
+  }
+  writeToData(dataJSON);
 } else if (userCommand === 'delete') {
   const deleteID = process.argv[3];
   delete dataJSON.notes[deleteID];
-
-  let stringified = JSON.stringify(dataJSON, null, 2)
-
-  fs.writeFile('data.json', stringified, 'utf8', err => {
-    if (err) throw err;
-  });
+  writeToData(dataJSON);
 } else if (userCommand === 'update') {
   const updateID = process.argv[3];
   const updateContent = process.argv[4];
-//dataJSON.notes[updateId] = newNote;
-// console.log(dataJSON.notes[updateId])
-  console.log(typeof updateID)
+  dataJSON.notes[updateID] = updateContent;
+  writeToData(dataJSON);
 }
 
-//
+function writeToData(dataJSON) {
+  let stringified = JSON.stringify(dataJSON, null, 2)
+  fs.writeFile('data.json', stringified, 'utf8', err => {
+    if (err) throw err;
+  });
+}
 
 // node app.js read
 //  iterates through .notes, console logs a template
 // node app.js create 'somesortofstring'
 //  increments nextID, adds a new property:value to the notes object
 // node app.js delete 1
-//
+//  deletes property from data.json and rewrites
 // node app.js update
-//
+//  replaces data.json object and rewrites
