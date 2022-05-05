6932c0caad52d28e14c554d93d048a2101cfc42b
Derek Lui <luiderek@outlook.com>
2022-05-05 22:38:43 +0000

    working on edit function

--- app.js
+++ app.js
@@ -10,5 +10,20 @@ if (userCommand === 'read') {
   }
 } else if (userCommand === 'create') {
   const newNote = process.argv[3];
-  console.log(newNote);
+  dataJSON.notes[dataJSON.nextId] = newNote;
+  dataJSON.nextId++;
+  let stringified = JSON.stringify(dataJSON, null, 2)
+  console.log(stringified);
+  // fs.writeFile();
 }
+
+//
+
+// node app.js read
+//  iterates through .notes, console logs a template
+// node app.js create 'somesortofstring'
+//  increments nextID, adds a new property:value to the notes object
+// node app.js delete
+//
+// node app.js update
+//
